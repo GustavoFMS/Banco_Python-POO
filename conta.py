1d925d59506f2c420acd04d3ee453c317f8b294f
GustavoFMS <sousa.gfm@gmail.com>
2023-01-11 11:41:45 -0400

    commit

---
+++ conta.py
@@ -0,0 +1,17 @@
+class Conta:
+    
+    def __init__(self, numero, titular, saldo, limite):
+        print(f"Construindo Objeto...{self}")
+        self.numero = numero
+        self.titular = titular
+        self.saldo = saldo
+        self.limite = limite
+        
+    def extrato(self):
+        print(f"O saldo de {self.saldo} do titular {self.titular}")
+
+    def depositar(self, valor):
+        self.saldo += valor
+
+    def sacar(self, valor):
+        self.saldo -= valor
