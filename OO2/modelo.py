a8cbc7a791c86e98ab2076f73a4fd1da498c68ea
GustavoFMS <sousa.gfm@gmail.com>
2023-01-30 08:47:24 -0400

    commit

--- OO2/modelo.py
+++ OO2/modelo.py
@@ -39,10 +39,21 @@ class Serie(Programa):
      def __str__(self):
         return f'{self._nome} - {self.ano} - {self.temporadas} temporadas - {self._likes} Likes'
 
-class Playlist(list):
+class Playlist:
     def __init__(self, nome, programas):
         self.nome = nome
-        super().__init__(programas)
+        self._programas = programas
+
+    def __getitem__(self, item):
+        return self._programas[item]
+
+    @property
+    def listagem(self):
+        return self._programas
+    @property
+    def tamanho(self):
+        return len(self._programas)
+    
 
 vingadores = Filme('vingadores - guerra infinita', 2018, 160)
 tmep = Filme ('Todo mundo em panico', 1999, 100)
@@ -64,7 +75,7 @@ demolidor.dar_likes()
 filmes_e_series = [vingadores, atlanta, demolidor, tmep]
 playlist_fim_de_semana = Playlist('fim de semana', filmes_e_series)
 
-print(f'Tamanho do playlist: {len(playlist_fim_de_semana)}')
+print(f'Tamanho do playlist: {len(playlist_fim_de_semana.listagem)}')
 
 for programa in playlist_fim_de_semana:
     print(programa)
